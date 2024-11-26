238df260e30dc29914ea0901571e879e66e24776
StefanHritcu <shritcu@proton.me>
2024-11-26 23:50:57 +0100

    add: more Tailwind.css BREAKPOINTS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,21 +15,24 @@ export default {
         verySmall: "340px",
         verySmall2: "362px",
         extraSmall: "400px",
-        sm: "640px",
-        md: "767px",
+        xs: "480px", // Aggiunta per dispositivi più piccoli
+        sm: "640px", // Tailwind CSS sm
+        md: "767px", // Tailwind CSS md
         md2: "818px",
         md3: "870px",
         md4: "940px",
-        lg: "1024px",
+        lg: "1024px", // Tailwind CSS lg
         lg2: "1100px",
-
-        //only for cloud bobbles
-        cloud1: "1100px",
-        cloud2: "1300px",
-        cloud3: "1400px",
-
-        xl: "1280px",
-        "2xl": "1536px",
+        xl: "1280px", // Tailwind CSS xl
+        xl2: "1400px", // Variabile per dimensioni maggiori
+        "2xl": "1536px", // Tailwind CSS 2xl
+        "3xl": "1600px", // Un altro breakpoint grande
+        "4xl": "1920px", // Large screens, full HD
+        "5xl": "2560px", // Ultra-wide screens
+        cloud1: "1100px", // Breakpoint personalizzato per cloud
+        cloud2: "1300px", // Breakpoint personalizzato per cloud
+        cloud3: "1400px", // Breakpoint personalizzato per cloud
+        // duplicati eliminati
       },
     },
     plugins: [],
